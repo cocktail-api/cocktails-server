dfd91feb2abd63831ef7414e15c462f0f8a9591e
Simon Levermann <simon-github@slevermann.de>
2020-04-12 15:23:11 +0200

    Make tests transactional

--- src/test/java/de/slevermann/cocktails/daos/IngredientDaoTest.java
+++ src/test/java/de/slevermann/cocktails/daos/IngredientDaoTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("dbtest")
+@Transactional
 public class IngredientDaoTest {
 
     @Autowired
